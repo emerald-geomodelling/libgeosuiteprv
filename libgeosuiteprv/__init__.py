105beb83d74e4df9d02ff5a11b08abcfc3b86075
Egil <egil.moller@freecode.no>
2021-09-29 09:29:50 +0200

    First import

---
+++ libgeosuiteprv/__init__.py
@@ -0,0 +1,57 @@
+from os.path import splitext, basename
+from itertools import count
+import numpy as np
+import datetime
+import time
+import pandas as pd
+import warnings
+from scipy.stats import linregress
+import zipfile
+import codecs
+import os
+import io
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def parse(input_filename, borehole_id=None):
+    if borehole_id is None:
+        borehole_id = input_filename.split("/")[-1].split(".", 1)[0]
+
+    df = pd.DataFrame()
+    comment_list = []
+    if isinstance(input_filename, str):
+        with open(input_filename, 'r', encoding='iso8859_10') as f:
+            lines = f.readlines()
+    else:
+        lines = codecs.getreader('utf8')(input_filename, errors='ignore').readlines()
+
+    firstline_list = lines[0][:-1].split()
+    main = [{"investigation_point": firstline_list[5],
+             'date': pd.to_datetime(firstline_list[2], format='%d.%m.%Y'),
+             "method_code": "core_sampling"}]
+    for l in lines[2:-1]:
+        values = l[:-1].split()
+        data_str = values[:12]
+        data_num = np.array(data_str, dtype=np.float)
+        comments = ' '.join(values[12:])
+        df = df.append(pd.Series(data_num), ignore_index=True)
+        comment_list.append(comments)
+    df.loc[:,'comments'] = comment_list
+    df = df.astype({0:'int32', 1:'int32'})
+
+    df = df.replace(0,np.nan).rename(columns={
+        0:'tube',
+        2:'depth',
+        3:'water_content_%',
+        8:'cu_kpa_undrained_shear_strength',
+        10:'unit_weight_kn_m3',
+        4:'plastic_limit',
+        5:'liquid_limit',
+        6:'cufc',
+        7:'curfc',
+    })
+
+    return [{"main": main,
+             "data": df}]
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import setuptools
+import os
+
+setuptools.setup(
+    name='libgeosuiteprv',
+    version='0.0.3',
+    description='Parser for the GeoSuite<tm> PRV export format',
+    long_description="""Parser for the GeoSuite<tm> PRV export format""",
+    long_description_content_type="text/markdown",
+    author='Egil Moeller, Craig William Christensen and others ',
+    author_email='em@emeraldgeo.no, cch@emeraldgeo.no',
+    url='https://github.com/emerald-geomodelling/libgeosuiteprv',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+)
